f3e30b7f5c88be7a75e210b6985fac62e4893b08
SasungKim <freeboy81520@gmail.com>
2020-06-14 07:21:21 -0400

    Initializing

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends React.Component {
+	render() {
+		return <h1>Game App</h1>;
+	}
+}
+
+export default App;
